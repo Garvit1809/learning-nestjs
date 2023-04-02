e84320d3c071eb6d89924c1260c2c2d70508bb9a
Garvit1809 <garvitvarshne18@gmail.com>
2023-04-02 10:24:06 +0530

    adding services and repo

--- messaging/src/messages/messages.controller.ts
+++ messaging/src/messages/messages.controller.ts
@@ -1,14 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Param, NotFoundException } from '@nestjs/common';
 import { Get, Post } from '@nestjs/common/decorators';
+import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessageService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messageService: MessageService;
+
+  constructor() {
+    this.messageService = new MessageService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messageService.findAll();
+  }
 
   @Post()
-  postMessages() {}
+  postMessages(@Body() body: CreateMessageDTO) {
+    console.log(body);
+    return this.messageService.create(body.content);
+  }
 
   @Get('/:id')
-  getMessage() {}
+  async getMessage(@Param('id') id: string) {
+    console.log(id);
+    const message = await this.messageService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found!!');
+    }
+
+    return message;
+  }
 }
